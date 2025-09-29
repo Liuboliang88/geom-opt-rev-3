e3359f53d1b699101a71a3be0f3d81b269edbb70
Liuboliang88 <liuboliang88@gmail.com>
2025-09-29 08:04:22 +0800

    Update New geomopt.cpp

--- New geomopt.cpp
+++ New geomopt.cpp
@@ -341,3 +341,33 @@ bool GeomOpt::run(double maxGrad,
 
     return false; // 未达成收敛
 }
+
+
+#pragma once
+
+#include "hessian.hpp"
+#include "cell.hpp"
+#include "rfo.hpp"
+#include <string>
+
+class GeomOpt {
+public:
+    GeomOpt(Cell& cell);
+
+    // Gaussian 4 条收敛判据
+    bool run(double gMaxTol = 4.5e-4,  // Max grad
+             double gRMSTol = 3.0e-4,  // RMS grad
+             double dMaxTol = 1.8e-3,  // Max disp
+             double dRMSTol = 1.2e-3,  // RMS disp
+             std::size_t maxCycle = 400);
+
+private:
+    Cell& cell;
+    Hessian hess;
+
+    std::vector<Matrix> xHist;
+    std::vector<Matrix> gHist;
+    std::vector<double> EHist;
+
+};
+
